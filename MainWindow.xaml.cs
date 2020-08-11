e2dde90b30669dd13dcbddb91aceccd21d02eba5
Inozpavel <Inozpavel@mail.ru>
2020-08-11 20:08:29 +0300

    Реализованы кнопки управления окном

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace WpfClipboardTextTyper
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void DragMoveApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
