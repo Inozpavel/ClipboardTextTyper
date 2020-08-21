d6552c84f41558e866feb20e50189b0a35754858
Inozpavel <Inozpavel@mail.ru>
2020-08-22 00:01:15 +0300

    Рефакторинг кода

--- ProjectFiles/MainWindow.xaml.cs
+++ ProjectFiles/MainWindow.xaml.cs
@@ -15,11 +15,19 @@ namespace WpfClipboardTextTyper
     public partial class MainWindow : Window
     {
         internal static Settings userSettings;
-        internal bool isAnimanting = false;
-        internal DoubleAnimation showHalfTimeInput = new DoubleAnimation(0, 0.6, new Duration(new TimeSpan(0, 0, 0, 0, 600)));
-        internal DoubleAnimation showTimeInput = new DoubleAnimation(0.6, 1, new Duration(new TimeSpan(0, 0, 0, 0, 300)));
-        internal DoubleAnimation hideHalfTimeInput = new DoubleAnimation(1, 0.6, new Duration(new TimeSpan(0, 0, 0, 0, 600)));
-        internal DoubleAnimation hideTimeInput = new DoubleAnimation(0.6, 0, new Duration(new TimeSpan(0, 0, 0, 0, 300)));
+        private bool _isAnimanting = false;
+
+        private readonly DoubleAnimation _showHalfTimeInput = new
+            DoubleAnimation(0, 0.6, new Duration(new TimeSpan(0, 0, 0, 0, 600)));
+
+        private readonly DoubleAnimation _showTimeInput = new
+            DoubleAnimation(0.6, 1, new Duration(new TimeSpan(0, 0, 0, 0, 300)));
+
+        private readonly DoubleAnimation _hideHalfTimeInput = new
+            DoubleAnimation(1, 0.6, new Duration(new TimeSpan(0, 0, 0, 0, 600)));
+
+        private readonly DoubleAnimation _hideTimeInput = new
+            DoubleAnimation(0.6, 0, new Duration(new TimeSpan(0, 0, 0, 0, 300)));
 
         public MainWindow()
         {
@@ -28,8 +36,12 @@ namespace WpfClipboardTextTyper
             InitializeComponent();
             DataContext = userSettings;
             Slider.Value = sliderTime;
-            showHalfTimeInput.Completed += new EventHandler((x, y) => TimeInput.BeginAnimation(OpacityProperty, showTimeInput));
-            hideHalfTimeInput.Completed += new EventHandler((x, y) => TimeInput.BeginAnimation(OpacityProperty, hideTimeInput));
+
+            _showHalfTimeInput.Completed += new EventHandler((x, y) =>
+            TimeInput.BeginAnimation(OpacityProperty, _showTimeInput));
+
+            _hideHalfTimeInput.Completed += new EventHandler((x, y) =>
+            TimeInput.BeginAnimation(OpacityProperty, _hideTimeInput));
         }
 
         private void DragMoveApp(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -45,30 +57,30 @@ namespace WpfClipboardTextTyper
 
         private void TBCaptionMouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            if (!isAnimanting)
+            if (_isAnimanting == false)
             {
-                isAnimanting = true;
+                _isAnimanting = true;
                 try
                 {
                     Task.Run(() =>
                     {
                         string caption = "";
                         Dispatcher.Invoke(() => caption = TBCaption.Text);
-                        int i = 1;
-                        while (i <= caption.Length)
+                        int count = 1;
+                        while (count <= caption.Length)
                         {
-                            Dispatcher.Invoke(() => TBCaption.Text = string.Join("", caption.Take(i).ToArray()));
-                            i++;
+                            Dispatcher.Invoke(() => TBCaption.Text = string.Join("", caption.Take(count)));
+                            count++;
                             Thread.Sleep(200);
                         }
                         Thread.Sleep(500);
-                        isAnimanting = false;
+                        _isAnimanting = false;
                     });
                 }
                 catch
                 {
                     TBCaption.Text = "ClipboardTextTyper";
-                    isAnimanting = false;
+                    _isAnimanting = false;
                 }
             }
         }
@@ -85,9 +97,9 @@ namespace WpfClipboardTextTyper
         private void ShowTimeInputChecked(object sender, RoutedEventArgs e)
         {
             if ((sender as RadioButton).IsChecked ?? false)
-                TimeInput.BeginAnimation(OpacityProperty, showHalfTimeInput);
+                TimeInput.BeginAnimation(OpacityProperty, _showHalfTimeInput);
             else
-                TimeInput.BeginAnimation(OpacityProperty, hideHalfTimeInput);
+                TimeInput.BeginAnimation(OpacityProperty, _hideHalfTimeInput);
 
             Settings.SaveSetting(userSettings);
         }
